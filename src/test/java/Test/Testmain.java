d4d97e922461c833831fb07a20fe683338f26cec
Jeyaprakash-Rajendran <rjprakash202@gmail.com>
2021-07-18 17:02:37 +0530

    basecommit

---
+++ src/test/java/Test/Testmain.java
@@ -0,0 +1,20 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Testmain {
+	
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Am in class1-method1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Am in class1-method2");
+	}
+
+}
---
+++ src/test/java/Test/Testmain1.java
@@ -0,0 +1,18 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Testmain1 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("Am in class2-method1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Am in class2-method2");
+	}
+}
